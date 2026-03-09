33d85ed1b883bba53cba5d58c04308d53fed7287
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-03-10 03:01:18 +0600

    adopted query added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { ResponseData } from "./types/response.js";
 import { pets } from "./data/pets.js";
 import type { Response, Request, Express } from "express";
 import type { Pet } from "./types/index.js";
+import type { GetPetQuery } from "./types/request.js";
 
 const app: Express = express();
 const PORT: number = Number(process.env.PORT) || 8000;
@@ -19,12 +20,33 @@ app.get("/", (req: Request, res: Response<ResponseData<undefined>>): void => {
 
 app.get(
   "/api/v1/pets",
-  (req: Request, res: Response<ResponseData<Pet>>): void => {
-    res.send({
-      success: true,
-      message: "Pets data retrieved successfully",
-      data: pets,
-    });
+  (
+    req: Request<{}, {}, {}, GetPetQuery>,
+    res: Response<ResponseData<Pet>>,
+  ): void => {
+    try {
+      const { adopted } = req.query;
+
+      let filteredPets: Pet[] = pets;
+
+      if (adopted) {
+        filteredPets = pets.filter(
+          (pet: Pet): boolean =>
+            pet.adopted === JSON.parse(adopted.toLowerCase()),
+        );
+      }
+
+      res.send({
+        success: true,
+        message: "Pets data retrieved successfully",
+        data: filteredPets,
+      });
+    } catch (error) {
+      res.status(500).send({
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
   },
 );
 
