82590f47ad9cec4fd49cbdb2e7076308fab37dd3
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-03-10 02:46:01 +0600

    get all pets data endpoint completed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { config } from "dotenv";
-import type { Response, Request, Express } from "express";
 import { ResponseData } from "./types/response.js";
+import { pets } from "./data/pets.js";
+import type { Response, Request, Express } from "express";
+import type { Pet } from "./types/index.js";
 
 const app: Express = express();
 const PORT: number = Number(process.env.PORT) || 8000;
@@ -14,6 +16,17 @@ app.get("/", (req: Request, res: Response<ResponseData<undefined>>): void => {
     message: "Welcome to the Pet Shop API",
   });
 });
+
+app.get(
+  "/api/v1/pets",
+  (req: Request, res: Response<ResponseData<Pet>>): void => {
+    res.send({
+      success: true,
+      message: "Pets data retrieved successfully",
+      data: pets,
+    });
+  },
+);
 
 app.use((req: Request, res: Response<ResponseData<undefined>>): void => {
   res.status(404).send({
