9d9b51b87854dd42239191466fec80bd0b111113
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-03-17 19:43:47 +0600

    middleware deleted

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { postUser } from "../controllers/usersController.js";
-import { validateReqBody } from "../utils/validateReqBody.js";
 
 export const usersRouter = Router();
 
-usersRouter.post("/", validateReqBody, postUser);
+usersRouter.post("/", postUser);
---
+++ src/validators/usersValidator.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const userSchemaZ = z.object(
+  {
+    
+  },
+  "User valid data required in the request body!",
+);
