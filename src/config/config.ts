b4478700237db11073af63a5a00d64f57fa75381
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-03-18 17:39:17 +0600

    modified

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,8 +7,5 @@ export const config = {
   DB_URI: process.env.DB_URI,
   PORT: process.env.PORT,
   CLIENT_URL: process.env.CLIENT_URL,
-  NODE_ENV: process.env.NODE_ENV,
   SALT_ROUND: process.env.SALT_ROUND,
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
 };
